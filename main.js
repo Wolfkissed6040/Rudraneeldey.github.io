db5bb2c93ee987a58eaa1942bb11089e88826d80
Rudraneel Dey <109857615+Wolfkissed6040@users.noreply.github.com>
2023-09-13 00:04:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".text",{
+    strings: ["ML enthusiast", "Web Developer", "Compititive Coder", "Graphic Designer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
